bb254f56e9329ed45f1b624158fd4df230ed89ab
BrandonEmbleton <brandonembleton04@gmail.com>
2026-03-25 20:03:24 -0400

    add Menu

--- bookTrackingSystem.cpp
+++ bookTrackingSystem.cpp
@@ -4,10 +4,40 @@
 #include "library.h"
 
 int main(){
+    char select;
+    bool running = true;
+
+    while(running){
+
+        printf("Menu: \n");
+        printf("1. Add Book \n");
+        printf("2. Remove Book \n");
+        printf("3. Update Book \n");
+        printf("4. View Book List \n");
+        printf("X. Exit \n");
+        printf("Select:");
+        std::cin >> select;
+
+        switch(select){
+            case '1':
+                //Add Book
+                break;
+            case '2':
+                //Remove Book
+                break;
+            case '3':
+                //Update Book
+                break;
+            case '4':
+                //Print Books
+                break;
+            case 'X':
+                running = false;
+                break;
+            default:
+                printf("Incorrect Selection");
+        }
+    }
     
-    //Test Code
-    // Library test = Library();
-    // test.addBook("title", "author", "description", 12, "horror", false);
-    // std::cout << test.books[0];
 }
 
--- library.h
+++ library.h
@@ -4,10 +4,14 @@ class Library {
 
     public:
         std::vector<Book> books;
+
         Library(){
 
         }
+
         void addBook(std::string title, std::string author, std::string description, int pages, std::string genre, bool read){
             this->books.push_back(Book(title, author, description, pages, genre, read));
         }
+
+        void removeBook(){}
 };
